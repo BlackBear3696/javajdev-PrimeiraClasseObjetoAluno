c91dea25ee4c6fe3dc91a14d0d084e0340cb1463
mlima3696 <mjuniorlima3@gmail.com>
2020-04-21 21:18:38 -0300

    Sobre o construtor do objeto

---
+++ PrimeiraClasseObjetoAluno/src/cursojava/classes/Aluno.java
@@ -0,0 +1,29 @@
+package cursojava.classes;
+
+//Está é mossa classe/objeto que o Aluno
+public class Aluno {
+
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCPF;
+	String nomeMae;
+	String nomePai;
+	String datamatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+	public Aluno() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
+	}
+	
+	public Aluno(String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+}
