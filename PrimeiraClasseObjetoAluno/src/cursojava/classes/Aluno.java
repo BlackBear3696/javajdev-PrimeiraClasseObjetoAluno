d84453827e880e3f9b03d4bba39149295ce8538e
mlima3696 <mjuniorlima3@gmail.com>
2020-04-22 02:07:02 -0300

    Metodos Getters e Setters

--- PrimeiraClasseObjetoAluno/src/cursojava/classes/Aluno.java
+++ PrimeiraClasseObjetoAluno/src/cursojava/classes/Aluno.java
@@ -2,16 +2,16 @@ package cursojava.classes;
 //Está é mossa classe/objeto que o Aluno
 public class Aluno {
 
-	public String nome;
-	public int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCPF;
-	String nomeMae;
-	String nomePai;
-	String datamatricula;
-	String nomeEscola;
-	String serieMatriculado;
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCPF;
+	private String nomeMae;
+	private String nomePai;
+	private String datamatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
 	
 	public Aluno() {
 		// TODO Auto-generated constructor stub
@@ -25,4 +25,92 @@ public class Aluno {
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
+	
+	// Veremos os metodos SETTRERS e GETTERS do objeto
+	// SET é para adicionar ou receber dados para os atributos
+	// GET é para resgatar ou obter o valor do atributos
+	// This faz referencia a classe dos atributos
+	
+	//Recebe dados
+	public void setNome(String nome) {		
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCPF() {
+		return numeroCPF;
+	}
+
+	public void setNumeroCPF(String numeroCPF) {
+		this.numeroCPF = numeroCPF;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDatamatricula() {
+		return datamatricula;
+	}
+
+	public void setDatamatricula(String datamatricula) {
+		this.datamatricula = datamatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	
 }
